b2e0910155d1f2960a74628cd065640ee16ed395
hmanjun <harshith.hm03@gmail.com>
2022-07-20 10:04:13 -0700

    Fix linking for testing

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,22 +1,22 @@
-const expect = require("mocha").expect
+const expect = require("chai").expect
 const Employee = require("../lib/Employee")
 
 describe("Employee", function(){
     it("takes and stores the parameter of id", function(){
         const id = 12
         worker = new Employee("name",id,"email")
-        expect(worker.id).to.be(id)
+        expect(worker.id).to.equal(id)
     })
 
     it("takes and stores the parameter of name", function(){
         const name = "James"
         worker = new Employee(name,12,"email")
-        expect(worker.name).to.be(name)
+        expect(worker.name).to.equal(name)
     })
 
     it("takes and stores the parameter of email", function(){
         const email = "sample@gmail.com"
         worker = new Employee("name",12,email)
-        expect(worker.email).to.be(email)
+        expect(worker.email).to.equal(email)
     })
 })
