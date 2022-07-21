c518d938ad1b13f473f9972290955b1c8debc378
hmanjun <harshith.hm03@gmail.com>
2022-07-20 17:26:45 -0700

    Add manager class

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,25 +4,25 @@ const Manager = require("../lib/Manager")
 describe("Manager", function(){
     it("takes and stores the parameter of id", function(){
         const id = 12
-        worker = new Employee("name",id,"email",101)
+        worker = new Manager("name",id,"email",101)
         expect(worker.id).to.equal(id)
     })
 
     it("takes and stores the parameter of name", function(){
         const name = "James"
-        worker = new Employee(name,12,"email",101)
+        worker = new Manager(name,12,"email",101)
         expect(worker.name).to.equal(name)
     })
 
     it("takes and stores the parameter of email", function(){
         const email = "sample@gmail.com"
-        worker = new Employee("name",12,email,101)
+        worker = new Manager("name",12,email,101)
         expect(worker.email).to.equal(email)
     })
 
     it("takes and stores the parameter of office number", function(){
         const officeNumber = 101
-        worker = new Manager("name",12,email,officeNumber)
+        worker = new Manager("name",12,"email",officeNumber)
         expect(worker.office).to.equal(officeNumber)
     })
 })
