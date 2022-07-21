5900176d2ebfe4f8101db112b74936188e1f0e7c
hmanjun <harshith.hm03@gmail.com>
2022-07-20 17:41:03 -0700

    Add test for intern class

---
+++ test/Intern.test.js
@@ -0,0 +1,28 @@
+const expect = require("expect.js")
+const Intern = require("../lib/Intern")
+
+describe("Intern", function(){
+    it("takes and stores the parameter of id", function(){
+        const id = 12
+        worker = new Intern("name",id,"email","school")
+        expect(worker.id).to.equal(id)
+    })
+
+    it("takes and stores the parameter of name", function(){
+        const name = "James"
+        worker = new Intern(name,12,"email","school")
+        expect(worker.name).to.equal(name)
+    })
+
+    it("takes and stores the parameter of email", function(){
+        const email = "sample@gmail.com"
+        worker = new Intern("name",12,email,"school")
+        expect(worker.email).to.equal(email)
+    })
+
+    it("takes and stores the parameter of github user", function(){
+        const school = "UCSC"
+        worker = new Intern("name",12,"email",school)
+        expect(worker.school).to.equal(school)
+    })
+})
